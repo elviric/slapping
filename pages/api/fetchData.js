a17e77fbdf230312556eaf122d96fa7a7f8de580
tumin-sheth <tumin.sheth@freightdeck.com>
2022-04-04 21:23:20 +0530

    connect mongo, add addr check

--- pages/api/fetchData.js
+++ pages/api/fetchData.js
@@ -1,5 +1,19 @@
+import clientPromise from "../../lib/mongodb";
 
+export default async function handler(req, res) {
+  const { address } = req.body;
+  const client = await clientPromise;
 
-export default function handler(req, res) {
- 
+  const db = client.db("HASH");
+
+  const data = await db.collection("addresses").findOne(
+    {address},
+    {projection: {"_id" : 0, "__v": 0}}
+  );
+  if (data) {
+    res.status(200).json(data);
+  }
+  res.status(404).json({"msg": "Not Found"});
+  
+   
 }
